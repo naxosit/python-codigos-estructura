83584b1e6787967ddb7eaaeacaaf24c5c9d5ae1c
naxosit <128820710+naxosit@users.noreply.github.com>
2023-04-14 12:55:10 -0400

    Clase 14/04/23

---
+++ arreglos-14-04.py
@@ -0,0 +1,32 @@
+# Crear un arreglo y ordenarlo de forma descendente
+# Luego ordenar ese mismo arreglo de forma aleatoria
+arr1= [0, 1, 2, 3]
+arr1.sort(reverse = True) #Ordena de descendente el arreglo
+print(arr1)
+import random
+random.shuffle(arr1) #Shuffle devuelve la secuencia con un argumento desordenado
+
+print(arr1) #Imprime el arreglo de un orden aleatorio
+    
+
+# Crear un algoritmo que permita guardar los nombres y el RUT de 5
+# personas sin perder ninguna de ellas. El usuario tiene que ingresar la
+# información de cada persona por consola.
+# Se debe imprimir los arrays de nombres y RUT para luego ordenarlas
+# alfabéticamente y ascendentemente respectivamente.
+#Datos
+'''
+nombre1= str(input("Nombre: "))
+rut1= str(input("Rut: "))
+nombre2= str(input("Nombre2: "))
+rut2= str(input("Rut2: "))
+nombre3= str(input("Nombre3: "))
+rut3= str(input("Rut3: "))
+nombre4= str(input("Nombre4: "))
+rut4= str(input("Rut4: "))
+nombre5= str(input("Nombre5: "))
+rut5= str(input("Rut5: "))
+
+array_nombres_ruts= [nombre1, rut1, nombre2, rut2, nombre3, rut3, nombre4, rut4, nombre5, rut5]
+print(array_nombres_ruts)
+'''
